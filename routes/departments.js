ce12a054ce93512957589de987948f60d0b2c044
Neilphpbeginner <lemmer.neil@gmail.com>
2022-05-01 16:51:16 +0200

    Added applicable get request to diff. departments

--- routes/departments.js
+++ routes/departments.js
@@ -2,6 +2,15 @@ const express = require("express");
 const Department = require("../mongooseScemas/departmentScema");
 const router = express.Router();
 
+router.get("/getAllDepartments", async (req, res, next) => {
+  try {
+    const findall = await Department.find();
+    res.send(findall);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.post("/addNewDepartment", async (req, res, next) => {
   const { newDepartment, activeDepartment } = req.body;
   try {
@@ -17,6 +26,24 @@ router.post("/addNewDepartment", async (req, res, next) => {
         res.send("New Department Added");
       }
     });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("getDepartments/:departmentName", async (req, res, next) => {
+  try {
+    const { departmentQuery } = req.params;
+    const findDepartment = await Department.findOne(
+      departmentQuery,
+      (error, department) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.send(department);
+        }
+      }
+    );
   } catch (error) {
     console.log(error);
   }
